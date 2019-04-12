216d6a4ac261fee47e04439f494e0848a326bf01
nadeem0035 <nadeem0035@gmail.com>
2019-04-12 15:26:49 +0500

    Update stripe.php
    Don't put live keys

--- application/config/stripe.php
+++ application/config/stripe.php
@@ -5,21 +5,21 @@ if (!defined('BASEPATH'))
 
 
 $config['stripe']['mode'] = 'test'; // test or live
-$config['stripe']['sk_test'] = 'sk_test_ZB6rrbObArTsMGGikfe7fAQV';
-$config['stripe']['pk_test'] = 'pk_test_xPZWkOwAOn3NQjrBINmr62fk';
+$config['stripe']['sk_test'] = '';
+$config['stripe']['pk_test'] = '';
 
-$config['stripe']['sk_live'] = 'sk_live_37aO3tlEfO1hQ8FHugBavSro';
-$config['stripe']['pk_live'] = 'pk_live_ptSeGbEXSyvqpDSHvaVhUOLB';
+$config['stripe']['sk_live'] = '';
+$config['stripe']['pk_live'] = '';
 $config['stripe']['currency'] = 'EUR';
 
 
 // Configuration options for CurlStripe
-$config['stripe_key_test_public'] = 'pk_test_xPZWkOwAOn3NQjrBINmr62fk';
-$config['stripe_key_test_secret'] = 'sk_test_ZB6rrbObArTsMGGikfe7fAQV';
+$config['stripe_key_test_public'] = '';
+$config['stripe_key_test_secret'] = '';
 
 
-$config['stripe_key_live_public'] = 'pk_live_3T39Obfaxx3twJTSa5yj5HqM';
-$config['stripe_key_live_secret'] = 'sk_live_6eaiwFf6HpYiT6DQATGFQHNl';
+$config['stripe_key_live_public'] = '';
+$config['stripe_key_live_secret'] = '';
 
 
 $config['stripe_test_mode'] = TRUE;
